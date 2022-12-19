f9f013a196ad939a1fc43bb30da7017c63496440
Eugene8615 <reznichenko8615@gmail.com>
2022-12-19 08:42:11 +0200

    Initial commit

---
+++ js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryDivREF = document.querySelector('.gallery');
+
+galleryDivREF.innerHTML = createGalleryMarkup(galleryItems);
+galleryDivREF.addEventListener('click', getFullImgLink);
+
+
+function createGalleryMarkup(items) {
+
+ return items.map(({original, preview, description}) => `<div class="gallery__item">
+  <a class="gallery__link">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+   </a>
+  </div>`).join('')
+};
+
+function getFullImgLink(event) {
+  if (event.target.nodeName !== 'IMG') return;
+
+  const instance = basicLightbox.create(`<img
+      class="gallery__image is-active"
+      src="${event.target.dataset.source}"
+    />`,).show();
+  
+  
+  
+};
+
+
+
+
+
+
+
